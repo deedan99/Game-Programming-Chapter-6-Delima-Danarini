199df353d00840f0d482433652dd8d16c35d582f
deedan99 <50495306+deedan99@users.noreply.github.com>
2021-09-25 21:49:23 +0700

    Add files via upload

---
+++ GameFlowManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameFlowManager : MonoBehaviour
+{
+    #region Singleton
+	public bool IsGameOver { get { return isGameOver; } }
+	private bool isGameOver = false;
+	private static GameFlowManager _instance = null;
+	public static GameFlowManager Instance{
+		get{
+			if (_instance == null){
+				_instance = FindObjectOfType<GameFlowManager>();
+				if (_instance == null){
+					Debug.LogError("Fatal Error: GameFlowManager not Found");
+				}
+			}
+			return _instance;
+		}
+	}
+	#endregion
+	private void Start(){
+		isGameOver = false;
+	}
+	
+	[Header("UI")]
+	public UIGameOver GameOverUI;
+	public void GameOver(){
+		isGameOver = true;
+		ScoreManager.Instance.SetHighScore();
+		GameOverUI.Show();
+	}	
+}
